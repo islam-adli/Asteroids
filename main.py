2be93dd2383d811f44a8549789867dfdfa7b7758
islam-adli <islamadli.2@gmail.com>
2026-05-04 12:18:47 +0300

    Fix hyprland conflict

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.NOFRAME)
     while True:
         log_state()
         for event in pygame.event.get():
