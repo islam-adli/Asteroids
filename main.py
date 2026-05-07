99c7e5d916ad165c5958461e8a04fc714924a320
islam-adli <islamadli.2@gmail.com>
2026-05-07 14:31:12 +0300

    add player.py and map controls

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import *
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -11,16 +12,22 @@ def main():
     pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.NOFRAME)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         pygame.time.Clock().tick(60)
         dt = pygame.time.Clock().tick(60) / 1000
-        print(dt)
 
 
 if __name__ == "__main__":
